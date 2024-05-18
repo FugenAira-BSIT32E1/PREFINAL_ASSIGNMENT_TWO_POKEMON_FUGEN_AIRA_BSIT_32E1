3947f2e09b20f7f4f698abe365f8bfe390ff7207
FugenAira-BSIT32E1 <153443814+FugenAira-BSIT32E1@users.noreply.github.com>
2024-05-18 15:17:45 +0800

    update cs

--- Prefinals_Fugen_Pokemon/Models/Pokemon.cs
+++ Prefinals_Fugen_Pokemon/Models/Pokemon.cs
@@ -1,6 +1,9 @@
-﻿namespace Prefinals_Fugen_Pokemon.Models
+﻿namespace Prefinals_Pokemon_Fugen.Models
 {
     public class Pokemon
     {
+        public string Name { get; set; }
+        public List<string> Abilities { get; set; }
+        public List<string> Moves { get; set; }
     }
 }
